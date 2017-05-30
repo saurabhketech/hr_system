428a0ee41e8e81b3213e4507e77217918172ee96
saurabh <saurabh>
2017-05-30 15:34:57 +0530

    modified inbox.js && automatic_tags.js

--- src/modules/automatic_tags.js
+++ src/modules/automatic_tags.js
@@ -1,23 +1,89 @@
+// import db from "../db"
+// module.exports = {
+//     tags: function(subject, email_date) {
+//         return new Promise((resolve, reject) => {
+//             let tag = "";
+//             db.Tag.findAll({ type: "Automatic" })
+//                 .then((data) => {
+//                     for (let i = 0; i < data.length; i++) {
+//                         let res = new RegExp(data[i].title, 'gi');
+//                         let tag = subject.match(res);
+//                         if (tag != null) {
+//                             resolve({ tags: tag, id: data[i].id });
+//                         } else {
+//                             db.Tag.findOne({ where: { from: { $lte: email_date }, $or: [{ to: { $gte: email_date } }] } })
+//                                 .then((tag) => {
+//                                      resolve({ tags: tag, id: data[i].id });
+//                                     console.log("+++++++++++++++++++")
+//                                     console.log(tag)
+//                                     console.log("+++++++++++++++++++")
+//                                 })
+//                         }
+//                         // } else {
+//                         //     if (i < data.length - 1) {
+//                         //         continue
+//                         //     } else {
+//                         //         resolve({ tags: null, id: null })
+//                         //     }
+//                         // }
+//                     }
+//                 })
+//         })
+//     }
+// };
+
+
+
 import db from "../db"
 module.exports = {
-    tags: function(subject) {
+    tags: function(subject, email_date) {
         return new Promise((resolve, reject) => {
             let tag = "";
-            db.Tag.findAll({
-                    type: "Automatic"
-                })
+            db.Tag.findAll({ type: "Automatic" })
                 .then((data) => {
                     for (let i = 0; i < data.length; i++) {
                         let res = new RegExp(data[i].title, 'gi');
-                        if (subject.match(res)) {
-                            resolve({ tags: subject.match(res), id: data[i].id });
+                        let tag = subject.match(res);
+                        if (tag != null) {
+                            resolve({ tags: tag, id: data[i].id });
                         } else {
-                            resolve({ tags: null, id: null })
+                            if (i < data.length - 1) {
+                                continue
+                            } else {
+                                resolve({ tags: null, id: null })
+                            }
                         }
                     }
                 })
-
         })
-
     }
 };
+
+
+
+// import db from "../db"
+// module.exports = {
+// tags: function(subject, email_date, callback) {
+//         db.Tag.findAll({
+//                 type: "Automatic"
+//             })
+//             .then((data) => {
+//                 if (data) {
+//                     for (let i = 0; i < data.length; i++) {
+//                         let res = new RegExp(data[i].title, 'gi'),
+//                             tag = subject.match(res);
+//                         callback(tag);
+//                     }
+//                 } else {
+//                     db.Tag.findOne({ where: { from: { $lte: email_date }, $or: [{ to: { $gte: email_date } }] } })
+//                         .then((docs, err) => {
+//                             if (docs) {
+//                                 console.log(docs);
+//                             } else {
+//                                 console.log(err);
+//                             }
+//                         });
+//                 }
+//             })
+//     }
+// };
